6ea23675616d971ab524cba66537f587e17e6816
chohyeokjun <chj3731@naver.com>
2025-10-05 15:59:56 +0900

    [level 0] Title: 할 일 목록, Time: 1.86 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer.stream().map(s -> s).toArray(String[]::new);
+    }
+}

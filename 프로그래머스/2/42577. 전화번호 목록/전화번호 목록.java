5400b08c65e6dd9ea5f16de8225a5e248398fdce
chohyeokjun <chj3731@naver.com>
2025-10-05 18:55:19 +0900

    [level 2] Title: 전화번호 목록, Time: 276.34 ms, Memory: 215 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        for (String s : phone_book) {
+            set.add(s);
+        }
+        for (String s : phone_book) {
+            for (int i = 1; i < s.length(); i++) {
+                if (set.contains(s.substring(0, i))) {
+                        return false;
+                    }
+            }
+        }   
+        return true;
+    }
+}

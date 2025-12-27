9df5a172c3abe23f84398ccd2f8b818fbe47c6de
chohyeokjun <chj3731@naver.com>
2025-12-27 23:41:48 +0900

    [level 1] Title: 체육복, Time: 0.15 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int rent = 0;
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
+        
+        for (int l : lost) {
+            lostSet.add(l);
+        }
+        
+        for (int r : reserve) {
+            if (lostSet.contains(r)) {
+                lostSet.remove(r);
+            } else {
+                reserveSet.add(r);
+            }
+        }
+        
+        for (int r : reserveSet) {
+            if (lostSet.contains(r-1)) {
+                lostSet.remove(r-1);
+            } else if (lostSet.contains(r+1)) {
+                lostSet.remove(r+1);
+            }
+        }
+        
+        return n - lostSet.size();
+    }
+}

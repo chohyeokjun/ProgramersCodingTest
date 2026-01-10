ba7693c48751840a9b0a3cac046e8cd010a37aee
chohyeokjun <chj3731@naver.com>
2026-01-10 15:31:13 +0900

    [Bronze V] Title: 등록, Time: 88 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int count = 5;
+        String id = "chj3731";
+        System.out.println(count);
+        System.out.println(id);
+    }
+}

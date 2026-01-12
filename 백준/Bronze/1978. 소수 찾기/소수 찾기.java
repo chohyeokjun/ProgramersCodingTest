8b67c58bd388c3ac5c881269e368ace83aea704a
chohyeokjun <chj3731@naver.com>
2026-01-12 16:01:55 +0900

    [Bronze II] Title: 소수 찾기, Time: 180 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -4,17 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] num = new int[N];
         int answer = 0;
-        
-        for (int i = 0; i < N; i++) {
-            num[i] = sc.nextInt();
-        }
-        
-        for (int i : num) {
+
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
             int count = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
                     count++;
                 }
             }

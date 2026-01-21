6a19a7aa4237afd6b56106e0b8de1c68ffe31f45
chohyeokjun <chj3731@naver.com>
2026-01-21 20:28:52 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 21584 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int[] arr = new int[X + 1];
+        
+        for (int i = 2; i <= X; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+            }
+        }
+
+        System.out.println(arr[X]);
+    }
+}

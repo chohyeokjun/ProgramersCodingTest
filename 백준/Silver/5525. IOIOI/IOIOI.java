a72220b27fa0ca6fc929cc69bb58b37344a406b9
chohyeokjun <chj3731@naver.com>
2026-01-28 20:58:03 +0900

    [Silver I] Title: IOIOI, Time: 416 ms, Memory: 29324 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,35 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        sc.nextLine();
-        String s = sc.nextLine();
-        
-        StringBuilder str1 = new StringBuilder();
-        str1.append("I");
-        
-        for (int i = 1; i <= N; i++) {
-            str1.append("OI");
-        }
-        
-        
-        int length = str1.length();
-        
-        String[] split = s.split("");
+        String s = sc.next();
         
         int count = 0;
-        for (int i = 0; i < M; i++) {
-            if (split[i].equals("I") && i + length <= M) {
-                boolean match = true;
-                for (int j = 0; j < length; j++) {
-                    if (!split[i + j].equals(str1.charAt(j) + "")) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
+        int oi_cnt = 0;
+        for (int i = 2; i < M; i++) {
+            if (s.charAt(i-2) == 'I' && s.charAt(i-1) == 'O' && s.charAt(i) == 'I') {
+                oi_cnt++;
+                if (oi_cnt >= N) {
                     count++;
+                    oi_cnt--;
                 }
+                i++;
+                continue;
             }
+            oi_cnt = 0;
         }
 
         System.out.println(count);

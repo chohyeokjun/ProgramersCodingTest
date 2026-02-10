ecce68422a96bcfc12f75c650dd9018d9fa860ea
chohyeokjun <chj3731@naver.com>
2026-02-10 22:36:57 +0900

    [level 0] Title: 소인수분해, Time: 6.60 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        if (n > 1) set.add(n);
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

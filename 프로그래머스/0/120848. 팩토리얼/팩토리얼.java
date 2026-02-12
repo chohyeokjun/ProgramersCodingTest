5ba1e40dd2012df42ee00dcd23dcc09fbb56310f
chohyeokjun <chj3731@naver.com>
2026-02-13 00:01:17 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int i = 1;
+        
+        while (answer <= n) {
+            i++;
+            answer *= i;
+            
+            if (answer > n) {
+                return i - 1;
+            }
+        }
+        return i - 1;
+    }
+}

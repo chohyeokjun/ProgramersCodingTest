f2f550d086f1cebb7545c0a354f38c1208c1c4e7
chohyeokjun <chj3731@naver.com>
2026-03-07 00:11:00 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        int g = gcd(a, b);
+        a /= g;
+        b /= g;
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+        return b == 1 ? 1 : 2;
+    }
+    
+    public int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}

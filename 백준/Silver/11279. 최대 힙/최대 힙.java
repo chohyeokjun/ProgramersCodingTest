a127b49de5ecaa12b5f0d1bb8eb56a423d13ff08
chohyeokjun <chj3731@naver.com>
2026-03-23 00:28:52 +0900

    [Silver II] Title: 최대 힙, Time: 868 ms, Memory: 112256 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}

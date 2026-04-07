2fc4c92cd7196237402930f006b2c87fef257bf6
chohyeokjun <chj3731@naver.com>
2026-04-07 23:35:04 +0900

    [Silver I] Title: 스티커, Time: 1560 ms, Memory: 314684 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[][] arr = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                arr[0][i] = sc.nextInt();
+            }
+            for (int i = 1; i <= n; i++) {
+                arr[1][i] = sc.nextInt();
+            }
+
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+
+            if (n >= 2) {
+                dp[0][2] = arr[0][2] + dp[1][1];
+                dp[1][2] = arr[1][2] + dp[0][1];
+            }
+
+            for (int i = 3; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+            }
+
+            sb.append(Math.max(dp[0][n], dp[1][n])).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
